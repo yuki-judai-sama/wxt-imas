a78e7465c765a63ff7fdfd137519327f9a7d0152
guoyanwu <yukijudai.sky@qq.com>
2025-09-16 12:48:47 +0800

    修改访问ip

--- src/utils/$axios.js
+++ src/utils/$axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 修正 baseURL，确保带有协议双斜杠
-axios.defaults.baseURL = "http://127.0.0.1:8115";
+axios.defaults.baseURL = "http://103.109.23.45:8115";
 
 export default axios;
 
